920dd11e4f29e5c401bcbf099ebd1d01f742d4e8
Jkmovie <124867430+Jkmovie@users.noreply.github.com>
2025-02-17 06:34:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # Telegram Bot Token
-MONGO_URI = os.getenv("MONGO_URI")  # MongoDB Connection String
+MONGO_URI = os.getenv("mongodb+srv://sampledhdj6:Y308LujZfMesIkpQ@cluster0.j9ha2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB Connection String
 TERABOX_API = os.getenv("TERABOX_API")  # Terabox API Key (Optional)
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))  # Admin Telegram ID (Default: 0)
-CHANNEL_ID = os.getenv("CHANNEL_ID")  # Forcing Subscription Channel ID
+ADMIN_ID = int(os.getenv("6644473960", ))  # Admin Telegram ID (Default: 0)
+CHANNEL_ID = os.getenv("2255827235")  # Forcing Subscription Channel ID
